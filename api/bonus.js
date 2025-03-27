d8c7daebef8d201ecd9765cefb3fb8a1bb8b7221
Vicke <spreadneedle@hotmail.com>
2025-03-27 11:05:29 +0200

    Update bonus.js

--- api/bonus.js
+++ api/bonus.js
@@ -1,25 +1,43 @@
 const casinoData = [
   {
-    casino_name: "NordicBet",
-    bonus: "100% up to $500 + 50 free spins",
-    referral_code: "GROK500",
-    geo: "Sweden",
-    updated: "2025-03-26"
+    casino_name: "Videoslots",
+    bonus: "100% up to €200 + 11 free spins",
+    wagering_requirement_bonus: "35x",
+    wagering_requirement_free_spins: "no wagering requirement",
+    free_spin_value: "€0.10/spin",
+    info: "This is a 'wager first' bonus, where the bonus is paid out in 10% increments as the deposit is wagered",
+    licenses: "Malta Gaming Authority, UK Gambling Commission, Swedish Gambling Authority, Alcohol and Gaming Commission of Ontario",
+    updated: "2025-03-27"
   },
   {
-    casino_name: "LeoVegas",
-    bonus: "200% up to $300",
-    referral_code: "GROK300",
-    geo: "Finland",
-    updated: "2025-03-26"
+    casino_name: "Boost Casino",
+    bonus: "100% 250 € asti + 50 free spins",
+    wagering_requirement_bonus: "35x",
+    wagering_requirement_free_spins: "30x",
+    free_spin_value: "€0.10/spin",
+    info: "Deposited funds are used first. The bonus is 'non-sticky'.",
+    licenses: "Estonian Tax and Customs Board",
+    updated: "2025-03-27"
   },
   {
-    casino_name: "Betsson",
-    bonus: "100% up to $400",
-    referral_code: "GROK400",
-    geo: "Sweden",
-    updated: "2025-03-26"
-  }
+    casino_name: "Pommi Casino",
+    bonus: "n/a",
+    wagering_requirement_bonus: "n/a",
+    wagering_requirement_free_spins: "n/a",
+    free_spin_value: "n/a",
+    info: "Pommi Casino offers no sign-up bonus.",
+    licenses: "Curacao Gaming Control Board",
+    updated: "2025-03-27"
+  },
+  {
+    casino_name: "Paratiisi Casino",
+    bonus: "300 free spins",
+    wagering_requirement_bonus: "n/a",
+    wagering_requirement_free_spins: "n/a",
+    free_spin_value: "€0.10/spin",
+    info: "100 free spins per day are handed out over 3 days",
+    licenses: "Curacao Gaming Control Board",
+    updated: "2025-03-27"
 ];
 
 export default function handler(req, res) {
