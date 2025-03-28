6dfcd2e420f0eba9ae1a92200af84323189b8af4
Vicke <spreadneedle@hotmail.com>
2025-03-28 21:52:11 +0200

    Update bonus.js

--- api/bonus.js
+++ api/bonus.js
@@ -35,9 +35,61 @@ const casinoData = [
     wagering_requirement_bonus: "n/a",
     wagering_requirement_free_spins: "n/a",
     free_spin_value: "€0.10/spin",
-    info: "100 free spins per day are handed out over 3 days",
+    info: "100 free spins per day are handed out over 3 days.",
     licenses: "Curacao Gaming Control Board",
     updated: "2025-03-27"
+  },
+  {
+    casino_name: "Bethard",
+    bonus: "100% up to €50 + 20 free spins",
+    wagering_requirement_bonus: "40x",
+    wagering_requirement_free_spins: "10x",
+    free_spin_value: "€0.20/spin",
+    info: "",
+    licenses: "Curacao Gaming Control Board",
+    updated: "2025-03-28"
+  },
+  {
+    casino_name: "Shokki Casino",
+    bonus: "n/a",
+    wagering_requirement_bonus: "n/a",
+    wagering_requirement_free_spins: "n/a",
+    free_spin_value: "n/a",
+    info: "Pommi Casino offers no sign-up bonus.",
+    licenses: "Curacao Gaming Control Board",
+    updated: "2025-03-28"
+  },
+  {
+    casino_name: "Lucky Trunk Casino",
+    bonus: "100% up to €250 + 50 free spins",
+    wagering_requirement_bonus: "70x",
+    wagering_requirement_free_spins: "35x",
+    free_spin_value: "€0.10/spin",
+    info: "Pommi Casino offers no sign-up bonus.",
+    licenses: "Malta Gaming Authority",
+    updated: "2025-03-28"
+  },
+  {
+    casino_name: "Hillo Casino",
+    bonus: "100 free spins",
+    wagering_requirement_bonus: "n/a",
+    wagering_requirement_free_spins: "50x",
+    free_spin_value: "€0.20/spin",
+    info: "50 free spins per day are handed out over 2 days.",
+    licenses: "Estonian Tax and Customs Board",
+    updated: "2025-03-28"
+  },
+  {
+    casino_name: "Teho Casino",
+    bonus: "150% up to €600",
+    wagering_requirement_bonus: "80x",
+    wagering_requirement_free_spins: "n/a",
+    free_spin_value: "n/a",
+    info: "The bonus is 'sticky'.",
+    licenses: "Malta Gaming Authority",
+    updated: "2025-03-28"
+  },
+      
 ];
 
 export default function handler(req, res) {
