be71c05d3b81a7050b8aba612896a3de9e0cc18f
spreadneedle <spreadneedle@hotmail.com>
2026-05-08 16:10:38 +0300

    [debug] Reveal full key

--- api/showkey.js
+++ api/showkey.js
@@ -3,24 +3,16 @@ const path = require('path');
 
 module.exports = (req, res) => {
   try {
-    // Read the _auth.js file directly
     const authPath = path.join(__dirname, '_auth.js');
     const content = fs.readFileSync(authPath, 'utf8');
-    
-    // Extract the key using regex
     const match = content.match(/API_KEY\s*=\s*['"]([^'"]+)['"]/);
     const key = match ? match[1] : 'NOT_FOUND';
     
+    // Return the FULL key (this is a debug endpoint, will be removed)
     res.json({
-      key_found: !!match,
-      key_length: key.length,
-      key_prefix: key.substring(0, 15),
-      key_suffix: key.substring(key.length - 6)
+      key: key
     });
   } catch (e) {
-    res.json({
-      error: e.message,
-      files_in_dir: fs.readdirSync(__dirname)
-    });
+    res.json({ error: e.message });
   }
 };
