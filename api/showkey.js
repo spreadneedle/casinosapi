b2c162c98ae73d10f056881c54b74dba329cfec8
spreadneedle <spreadneedle@hotmail.com>
2026-05-08 16:09:25 +0300

    [debug] Read auth file directly

--- api/showkey.js
+++ api/showkey.js
@@ -1,35 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (req, res) => {
-  // Try to read the key from the auth module by monkey-patching
-  const originalSend = res.send;
-  const originalJson = res.json;
-  let capturedStatus = null;
-  let capturedBody = null;
-  
-  res.json = function(body) {
-    capturedBody = body;
-    return originalJson.call(this, body);
-  };
-  
-  res.status = function(code) {
-    capturedStatus = code;
-    return this;
-  };
-  
-  const auth = require('./_auth');
-  
-  // Call requireApiKey with a fake request
-  const result = auth.requireApiKey(
-    { headers: { 'x-api-key': 'test-key-123' }, query: {} },
-    res
-  );
-  
-  res.json = originalJson;
-  res.status = function(code) { res.statusCode = code; return this; };
-  
-  res.json({
-    auth_result: result,
-    captured_status: capturedStatus,
-    captured_body: capturedBody,
-    received_key: 'test-key-123'
-  });
+  try {
+    // Read the _auth.js file directly
+    const authPath = path.join(__dirname, '_auth.js');
+    const content = fs.readFileSync(authPath, 'utf8');
+    
+    // Extract the key using regex
+    const match = content.match(/API_KEY\s*=\s*['"]([^'"]+)['"]/);
+    const key = match ? match[1] : 'NOT_FOUND';
+    
+    res.json({
+      key_found: !!match,
+      key_length: key.length,
+      key_prefix: key.substring(0, 15),
+      key_suffix: key.substring(key.length - 6)
+    });
+  } catch (e) {
+    res.json({
+      error: e.message,
+      files_in_dir: fs.readdirSync(__dirname)
+    });
+  }
 };
