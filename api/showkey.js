1801eee66f3e5c838c45d7ba0d2bfe7a515cd5e2
spreadneedle <spreadneedle@hotmail.com>
2026-05-08 16:07:44 +0300

    [debug] Fix showkey error handling

--- api/showkey.js
+++ api/showkey.js
@@ -1,10 +1,15 @@
-const crypto = require('crypto');
-const { API_KEY } = require('./_auth');
-
 module.exports = (req, res) => {
-  res.json({
-    key_hash: crypto.createHash('sha256').update(API_KEY).digest('hex').substring(0, 16),
-    key_length: API_KEY.length,
-    key_prefix: API_KEY.substring(0, 10)
-  });
+  try {
+    const auth = require('./_auth');
+    res.json({
+      exports: Object.keys(auth),
+      hasApiKey: 'API_KEY' in auth,
+      hasRequireApiKey: 'requireApiKey' in auth
+    });
+  } catch (e) {
+    res.json({
+      error: e.message,
+      stack: e.stack.split('\n').slice(0, 3)
+    });
+  }
 };
