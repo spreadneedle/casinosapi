614569891f94b96425b8378801f068f4a327e308
spreadneedle <spreadneedle@hotmail.com>
2026-05-08 16:08:31 +0300

    [debug] Check what key auth expects

--- api/showkey.js
+++ api/showkey.js
@@ -1,15 +1,35 @@
 module.exports = (req, res) => {
-  try {
-    const auth = require('./_auth');
-    res.json({
-      exports: Object.keys(auth),
-      hasApiKey: 'API_KEY' in auth,
-      hasRequireApiKey: 'requireApiKey' in auth
-    });
-  } catch (e) {
-    res.json({
-      error: e.message,
-      stack: e.stack.split('\n').slice(0, 3)
-    });
-  }
+  // Try to read the key from the auth module by monkey-patching
+  const originalSend = res.send;
+  const originalJson = res.json;
+  let capturedStatus = null;
+  let capturedBody = null;
+  
+  res.json = function(body) {
+    capturedBody = body;
+    return originalJson.call(this, body);
+  };
+  
+  res.status = function(code) {
+    capturedStatus = code;
+    return this;
+  };
+  
+  const auth = require('./_auth');
+  
+  // Call requireApiKey with a fake request
+  const result = auth.requireApiKey(
+    { headers: { 'x-api-key': 'test-key-123' }, query: {} },
+    res
+  );
+  
+  res.json = originalJson;
+  res.status = function(code) { res.statusCode = code; return this; };
+  
+  res.json({
+    auth_result: result,
+    captured_status: capturedStatus,
+    captured_body: capturedBody,
+    received_key: 'test-key-123'
+  });
 };
